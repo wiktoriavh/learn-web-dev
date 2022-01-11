4d55a7484ac43d2c120106cf86efa4e1f83d4c95
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-01-11 20:56:35 +0100

    🚨 fix lint warnings

--- components/List.tsx
+++ components/List.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+
 import type { Entry } from '../utils/sources';
 
 export function List({
@@ -8,7 +9,6 @@ export function List({
   entries: Entry[];
   type: string;
 }): JSX.Element {
-  console.log('hello?');
   return (
     <section className="w-[500px] mx-auto mb-20">
       <h3 className="text-2xl my-4 text-pink-400 font-bold">{type}</h3>
