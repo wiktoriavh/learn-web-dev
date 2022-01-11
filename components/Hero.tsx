52c2ebcf6da2871216c1f506f95a3288129a81ad
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-01-11 19:51:16 +0100

    ✨ Hero (header) component

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+export function Hero(): JSX.Element {
+  return (
+    <div className="h-[35rem] py-28 bg-gradient-radial-top from-[#581C87] via-[#5B1535] to-dark ">
+      <div className="md:container mx-auto">
+        <h1 className="text-6xl text-pink-50 text-center font-light ">
+          Learn Web Development from Scratch
+        </h1>
+        <h2 className="text-2xl text-center text-pink-50 my-20">
+          Das ist nur eine kurze Beschreibung zur Einführung, blah blah blah...
+        </h2>
+      </div>
+    </div>
+  );
+}
