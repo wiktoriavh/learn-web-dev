fbc0f75dfd1185cf878479d0a7d6d7a55bca11a7
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-01-11 20:41:27 +0100

    ✨ Index Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
 import type { NextPage } from 'next';
+
+import { Footer } from '../components/Footer';
 import { Hero } from '../components/Hero';
-import { Logo } from '../components/Logo';
+import { List } from '../components/List';
+import { sources } from '../utils/sources';
 
 const Home: NextPage = () => {
   return (
     <div className="bg-dark mx-auto text-light-grey min-h-screen">
       <Hero />
-      <Logo />
+      {sources.map((source) => {
+        return <List {...source} />;
+      })}
+      <Footer />
     </div>
   );
 };
