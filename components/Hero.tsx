e7d21f1a543e5bad4ad968814a5177231cab8cb3
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-01-13 18:50:22 +0100

    ✨ use arguments for heading/subheading

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,21 @@
-export function Hero(): JSX.Element {
+export function Hero({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle?: string;
+}): JSX.Element {
   return (
     <div className="min-h-96 max-h-screen py-28 px-6 mb-6 bg-gradient-radial-top from-[#581C87] via-[#5B1535] to-dark">
       <div className="md:container mx-auto">
         <h1 className="text-5xl text-pink-50 text-center font-light ">
-          Learn Web Development from Scratch
+          {title}
         </h1>
-        <h2 className="text-2xl md:text-center text-pink-50 my-20 md:w-3/5 mx-auto">
-          A compilation of resources from entry level coding to advanced development
-          techniques, curated by one of the largest web development communities on Discord.
-        </h2>
+        {subtitle && (
+          <h2 className="text-2xl md:text-center text-pink-50 my-20 md:w-3/5 mx-auto">
+            {subtitle}
+          </h2>
+        )}
       </div>
     </div>
   );
