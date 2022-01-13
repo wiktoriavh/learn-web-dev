8067c356db749f60ad1b0bd23f2f3ed0179a8f1c
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-01-13 18:14:31 +0100

    💄 fix hero text overlap

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,8 @@
 export function Hero(): JSX.Element {
   return (
-    <div className="h-[36rem] py-28 px-6 mb-6 bg-gradient-radial-top from-[#581C87] via-[#5B1535] to-dark">
+    <div className="min-h-96 max-h-screen py-28 px-6 mb-6 bg-gradient-radial-top from-[#581C87] via-[#5B1535] to-dark">
       <div className="md:container mx-auto">
-        <h1 className="text-6xl text-pink-50 text-center font-light ">
+        <h1 className="text-5xl text-pink-50 text-center font-light ">
           Learn Web Development from Scratch
         </h1>
         <h2 className="text-2xl md:text-center text-pink-50 my-20 md:w-3/5 mx-auto">
