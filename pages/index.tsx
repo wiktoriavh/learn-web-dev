e3a958a21a69235974aa4e4a18949ef04549aeb4
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-01-13 18:51:23 +0100

    ✨ fill Hero props

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,12 @@ import { sources } from '../utils/sources';
 // eslint-disable-next-line react/function-component-definition
 const Home: NextPage = () => {
   return (
-    <div className="bg-dark mx-auto text-light-grey min-h-screen">
-      <Hero />
+    <div className="mx-auto text-light-grey min-h-screen">
+      <Hero
+        title="Learn Web Development from Scratch"
+        subtitle="A compilation of resources from entry level coding to advanced development
+          techniques, curated by one of the largest web development communities on Discord."
+      />
       {sources.map((source) => {
         return <List key={source.type} {...source} />;
       })}
