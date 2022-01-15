07df7f94f1a88289a4116f65007f52004f0c55b8
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-01-15 21:15:47 +0100

    ✨ render description per list

--- components/List.tsx
+++ components/List.tsx
@@ -5,13 +5,16 @@ import type { Entry } from '../utils/sources';
 export function List({
   entries,
   type,
+  description,
 }: {
   entries: Entry[];
   type: string;
+  description?: string;
 }): JSX.Element {
   return (
     <section className="md:w-[500px] mx-auto mb-20 w-auto px-6 md:px-0">
       <h3 className="text-2xl my-4 text-pink-400 font-bold">{type}</h3>
+      {description && <p className="text-lg pb-2">{description}</p>}
       <ul>
         {entries?.map((entry) => (
           <li key={entry.name} className="my-2 text-lg">
