8757348878ae30cf81a2e629a5eb153507fc7438
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-04-09 21:52:13 +0200

    🐛 remove external script file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import type { AppProps } from 'next/app';
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <script src="https://cdn.jsdelivr.net/gh/greentfrapp/pocoloco@minigl/minigl.js" />
       <Component {...pageProps} />
     </>
   );
