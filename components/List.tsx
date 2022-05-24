88fa38f01d1b7fad53e98c21aab8128ec138c5f6
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-05-24 20:30:16 +0200

    ♻️ use the listitem component

--- components/List.tsx
+++ components/List.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 import type { Entry } from '../utils/sources';
+import { ListItem } from './ListItem/ListItem';
 
 export function List({
   entries,
@@ -12,20 +13,27 @@ export function List({
   description?: string;
 }): JSX.Element {
   return (
-    <section className="md:w-[500px] mx-auto mb-20 w-auto px-6 md:px-0">
-      <h3 className="text-2xl my-4 text-pink-400 font-bold">{type}</h3>
+    <section className="md:w-[636px] mx-auto mb-20 w-auto px-6 md:px-0">
+      <h3 className="text-2xl my-4 text-pink-400 font-bold text-center">
+        {type}
+      </h3>
       {description && <p className="text-lg pb-2">{description}</p>}
       <ul>
         {entries?.map((entry) => (
           <li key={entry.name} className="my-2 text-lg">
-            <Link href={entry.url}>
+            {/* <Link href={entry.url}>
               <a className="text-pink-200 hover:underline hover:underline-offset-2">
                 {entry.name}
               </a>
             </Link>
             {entry.description && (
               <p className="pl-4  my-2 mb-4">{entry.description}</p>
-            )}
+            )} */}
+            <ListItem
+              title={entry.name}
+              url={entry.url}
+              description={entry.description}
+            />
           </li>
         ))}
       </ul>
