53f55ebd94a3efebe712fc82f47684fe8856533a
Malte <malte.kluft@gmail.com>
2022-06-09 16:23:11 +0200

    ✨ add anchor ids to each list item

--- components/ListItem/ListItem.tsx
+++ components/ListItem/ListItem.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { formatSlug } from '~/format';
 
 import type { ListItemProps } from './types';
 
@@ -8,10 +9,10 @@ export function ListItem({
   url,
 }: ListItemProps): JSX.Element {
   return (
-    <div className="border-[1px] border-pink-50 rounded">
+    <div id={formatSlug(title)} className="border-[1px] border-pink-50 rounded">
       <Link href={url}>
         <a className="w-100 h-100 p-2.5 flex flex-col gap-2.5 hover:bg-black-800 ">
-          <span className="text-pink-200 text-lg">{title}</span>
+          <span className="text-lg text-pink-200">{title}</span>
           <p className="text-sm ">{description}</p>
         </a>
       </Link>
