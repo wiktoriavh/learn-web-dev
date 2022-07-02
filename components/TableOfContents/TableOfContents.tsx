ef9b1583032819c6009998c0a12931446d426073
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-07-02 18:07:05 +0200

    🎨 use full names for variables

--- components/TableOfContents/TableOfContents.tsx
+++ components/TableOfContents/TableOfContents.tsx
@@ -11,8 +11,10 @@ export function TableOfContents({
   const [active, setActive] = useState(-1);
 
   useEffect(() => {
-    const activeSource = sources.findIndex((s) =>
-      s.entries.some((e) => `#${formatSlug(e.name)}` === window.location.hash)
+    const activeSource = sources.findIndex((source) =>
+      source.entries.some(
+        (entry) => `#${formatSlug(entry.name)}` === window.location.hash
+      )
     );
 
     if (activeSource !== -1) {
@@ -22,16 +24,16 @@ export function TableOfContents({
 
   return (
     <nav className="sticky mb-16 xl:mt-4 xl:ml-auto top-20 xl:max-w-max">
-      <h2 className="mb-4 ml-2 text-2xl font-bold text-center text-pink-400 md:text-left">
+      <h3 className="mb-4 ml-2 text-2xl font-bold text-center text-pink-400 md:text-left">
         Table of contents
-      </h2>
+      </h3>
       <menu className="space-y-1">
-        {sources.map((source, i) => (
+        {sources.map((source, index) => (
           <li key={source.type}>
             <div
               className={clsx(
                 'transition-[padding,margin,border] border rounded',
-                i === active
+                index === active
                   ? 'p-2 text-pink-400 border-white'
                   : 'mx-2 text-pink-200 border-transparent'
               )}
@@ -39,11 +41,11 @@ export function TableOfContents({
               <button
                 type="button"
                 onClick={() => {
-                  setActive(i === active ? -1 : i);
+                  setActive(index === active ? -1 : index);
                 }}
                 className={clsx(
                   'flex w-full items-center justify-between gap-2 p-2 rounded cursor-pointer hover:bg-black-800',
-                  i === active && 'bg-black-800'
+                  index === active && 'bg-black-800'
                 )}
               >
                 {source.type}
@@ -52,7 +54,7 @@ export function TableOfContents({
                   xmlns="http://www.w3.org/2000/svg"
                   className={clsx(
                     'w-5 h-5 group-open:rotate-180 group-hover:opacity-100',
-                    i === active ? 'opacity-100' : 'opacity-0'
+                    index === active ? 'opacity-100' : 'opacity-0'
                   )}
                   viewBox="0 0 20 20"
                   fill="currentColor"
@@ -67,7 +69,7 @@ export function TableOfContents({
               <menu
                 className={clsx(
                   'p-2 text-pink-200 list-inside duration-300',
-                  i === active
+                  index === active
                     ? 'opacity-100 py-2 transition-[padding,opacity]'
                     : 'opacity-0 py-0 transition-[padding] pointer-events-none'
                 )}
@@ -76,7 +78,7 @@ export function TableOfContents({
                   <li
                     className={clsx(
                       'transition-[line-height,margin] duration-300',
-                      i === active
+                      index === active
                         ? 'leading-normal mt-2 first:mt-0'
                         : 'leading-[0] mt-0'
                     )}
@@ -86,7 +88,7 @@ export function TableOfContents({
                       {/* As clean as this is, we might want to use a JS-based approach for the scrolling so that we can add a slight offset*/}
                       <a
                         className="transition-[margin] block hover:ml-2"
-                        tabIndex={i === active ? 0 : -1}
+                        tabIndex={index === active ? 0 : -1}
                       >
                         {entry.name}
                       </a>
