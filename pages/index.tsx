676c8958458dd3d7c3cef10b3b0751de747bfeec
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-09-02 20:58:34 +0200

    🔥 remove table of contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,16 +19,11 @@ const Home: NextPage = () => {
           subtitle="A compilation of resources from entry level coding to advanced development
           techniques, curated by one of the largest web development communities on Discord."
         />
-        <div className="grid xl:grid-cols-[1fr_max-content_1fr]">
-          <aside className="container xl:max-w-full">
-            <TableOfContents sources={sources} />
-          </aside>
-          <main className="container">
-            {sources.map((source) => {
-              return <List key={source.type} {...source} />;
-            })}
-          </main>
-        </div>
+        <main className="container my-auto">
+          {sources.map((source) => {
+            return <List key={source.type} {...source} />;
+          })}
+        </main>
         <Footer />
       </div>
     </>
