6f76d73410b3827d86cfa9469006d2fe7a3bebd8
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-11-03 18:09:29 +0100

    🐛 fix next/link migration

--- components/ListItem/ListItem.tsx
+++ components/ListItem/ListItem.tsx
@@ -10,11 +10,12 @@ export function ListItem({
 }: ListItemProps): JSX.Element {
   return (
     <div id={formatSlug(title)} className="border-[1px] border-pink-50 rounded">
-      <Link href={url}>
-        <a className="w-100 h-100 p-2.5 flex flex-col gap-2.5 hover:bg-black-800 ">
-          <span className="text-lg text-pink-200">{title}</span>
-          <p className="text-sm ">{description}</p>
-        </a>
+      <Link
+        href={url}
+        className="w-100 h-100 p-2.5 flex flex-col gap-2.5 hover:bg-black-800 "
+      >
+        <span className="text-lg text-pink-200">{title}</span>
+        <p className="text-sm ">{description}</p>
       </Link>
     </div>
   );
